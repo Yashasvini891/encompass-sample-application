0c6af5533d6b01874fb862b4cdcd535d2975d904
Yashasvini891 <yashasvini.kotakonda@bhrish.com>
2026-06-01 22:24:48 +0530

    missed app in importing

--- epc-sample-app/src/services/authService.ts
+++ epc-sample-app/src/services/authService.ts
@@ -1,12 +1,12 @@
 export const getAccessToken = async () => {
-  const url = import.meta.env.VITE_OAUTH_URL;
+  const url = import.meta.env.VITE_APP_OAUTH_URL;
 
   const body = new URLSearchParams();
 
   body.append("grant_type", "client_credentials");
-  body.append("client_id", import.meta.env.VITE_CLIENT_ID);
-  body.append("client_secret", import.meta.env.VITE_CLIENT_SECRET);
-  body.append("scope", import.meta.env.VITE_SCOPE);
+  body.append("client_id", import.meta.env.VITE_APP_CLIENT_ID);
+  body.append("client_secret", import.meta.env.VITE_APP_CLIENT_SECRET);
+  body.append("scope", import.meta.env.VITE_APP_SCOPE);
 
   const response = await fetch(url, {
     method: "POST",
